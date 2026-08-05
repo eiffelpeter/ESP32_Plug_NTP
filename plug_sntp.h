712d48f4bb48baceedf1fc83f85b9f82cd2f6189
peterwu <peterwu@johnsonfitness.com>
2026-08-05 11:07:51 +0800

    Fix relay off again when wifi not connected and weekend_run false

--- plug_sntp.h
+++ plug_sntp.h
@@ -19,7 +19,7 @@ String current_time;
 static int32_t hour;
 static int32_t minute;
 static int32_t second;
-int day_of_week;
+static int day_of_week = -1; // if default 0, will cause relay off again when wifi not connected and weekend_run false
 
 
 
